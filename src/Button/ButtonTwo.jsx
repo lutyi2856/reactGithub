490fe69b0b1897c62dbc5dd23c1051724e9a6578
Сергей Сычёв <lutyi2856@gmail.com>
2024-02-09 18:02:00 +0300

    button-4

--- src/Button/ButtonTwo.jsx
+++ src/Button/ButtonTwo.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 
-export class Car extends React.Component {
+export class ButtonTwo extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { test: "Title", two: "two" };
+    // this.action = this.action.bind(this);
   }
   componentDidMount() {
     console.log("Сейчас");
@@ -10,15 +12,19 @@ export class Car extends React.Component {
   componentWillUnmount() {
     console.log("ушел");
   }
+  action = () => {
+    this.setState({ test: "not" });
+  };
   render() {
     return (
       <button
         href="#"
-        onClick={this.props.action}
+        onClick={this.action}
         className={`btn ${this.props.type}`}
       >
         <span>{this.props.icon}</span>
-        <span onClick={this.props.action}>{this.props.title}</span>
+        <span>{this.state.test}</span>
+        <p>{this.state.two}</p>
       </button>
     );
   }
--- src/Hero-content/Hero-content.jsx
+++ src/Hero-content/Hero-content.jsx
@@ -2,20 +2,77 @@ import "./hero-content.css";
 import MyAlert from "../MyAlert/MyAlert";
 import Button from "../Button/Button";
 import { SlLike } from "react-icons/sl";
-import { Car } from "../Button/ButtonTwo";
-
-const action = () => {
-  console.log(this);
-};
+import { ButtonTwo } from "../Button/ButtonTwo";
+import { useState } from "react";
 
 const HeroContent = () => {
+  const [title, setTitle] = useState("begine");
+  const [isready, setIsReady] = useState(false);
+  const [isready1, setIsReady1] = useState(false);
+  const [isready2, setIsReady2] = useState(false);
+  const [isready3, setIsReady3] = useState(false);
+  const action = () => {
+    setTitle("end");
+    setIsReady(!isready);
+  };
+  const action1 = () => {
+    setTitle("end1");
+    setIsReady1(!isready1);
+  };
+  const action2 = () => {
+    setTitle("end2");
+    setIsReady2(!isready2);
+  };
+  const action3 = () => {
+    setTitle("end3");
+    setIsReady3(!isready3);
+  };
   return (
     <div className="hero-content">
-      <h1 className="hero-title">Home</h1>
-      <p className="hero-description">This is a home page.</p>
-      <MyAlert title="главная страница" description="ntcn ntrcn" type="error" />
-      <MyAlert description="илоаимоифуидшфуишмифуд" type="success" />
-      <Car type="primary" click="нажатие" action={action} icon={<SlLike />} />
+      {isready && (
+        <MyAlert title={"кнопка-1"} description={"error-style"} type="error" />
+      )}
+      {isready1 && (
+        <MyAlert
+          title={"кнопка-2"}
+          description={"primary-style"}
+          type="primary"
+        />
+      )}
+      {isready2 && (
+        <MyAlert
+          title={"кнопка-3"}
+          description={"success-style"}
+          type="success"
+        />
+      )}
+      {isready3 && (
+        <MyAlert
+          title={"кнопка-4"}
+          description={"alerta-style"}
+          type="warning"
+        />
+      )}
+      <Button type="error" click="нажатие" action={action} icon={<SlLike />} />
+
+      <Button
+        type="primary"
+        click="нажатие-1"
+        action={action1}
+        icon={<SlLike />}
+      />
+      <Button
+        type="btn-default"
+        click="нажатие-3"
+        action={action2}
+        icon={<SlLike />}
+      />
+      <Button
+        type="warning"
+        click="нажатие-4"
+        action={action3}
+        icon={<SlLike />}
+      />
     </div>
   );
 };
